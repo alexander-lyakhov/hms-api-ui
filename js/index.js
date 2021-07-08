4a7da12c09bc22002aa53546c53949226b089839
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2021-07-08 11:13:22 +0300

    loot of changes

--- js/index.js
+++ js/index.js
@@ -1,58 +1,12 @@
 ﻿(function() {
-  const echo00 = {
-    init() {
-      this.terminal = document.querySelector('.terminal')
-      console.log('init', this.terminal)
-    },
-
-    print(data = {}, type = 'info') {
-      const text = typeof data === 'string' ? data : data.text
-
-      this.terminal.insertAdjacentHTML('beforeend', `
-        <pre class="${ type }">${ text }</pre>
-      `)
-
-      return this
-    },
-
-    printStatus(data = {}, type = 'success') {
-      this.terminal.insertAdjacentHTML('beforeend', `
-        <pre class="status ${ type }">
-          <span>${data.statusCode}</span>
-          <span>${ type.toUpperCase() }</span>
-        </pre>
-      `)
-
-      return this
-    },
-
-    clear() {
-      this.terminal.innerHTML = ''
-    },
-
-    title(data = {}) {
-      return this.print(data, 'title')
-    },
-
-    text(data = {}) {
-      return this.print(data)
-    },
-
-    success(data = {}) {
-      return this.printStatus(data, 'success')
-    },
-
-    error(data = {}) {
-      return this.printStatus(data, 'error')
-    },
-  }
-
   const echo = function() {
     const terminal = document.querySelector('.terminal')
     const _this = {}
 
-    function print(data = {}, type = 'info') {
-      const text = typeof data === 'string' ? data : data.text
+    function print(data = '', type = 'info') {
+      const text = typeof data === 'string'
+        ? data
+        : JSON.stringify(data.res || data, null, 4)
 
       terminal.insertAdjacentHTML('beforeend', `
         <pre class="${ type }">${ text }</pre>
@@ -73,14 +27,19 @@
     }
 
     Object.assign(_this, {
-      clear:
-        () => terminal.innerHTML = '',
+      clear: () => {
+        terminal.innerHTML = ''
+        return _this
+      },
 
       title:
         (data = {}) => print(data, 'title'),
 
       text:
         (data = {}) => print(data),
+
+      status:
+        (data = {}) => _this[data.ok ? 'success' : 'error'](data),
 
       success:
         (data = {}) => printStatus(data, 'success'),
@@ -92,11 +51,23 @@
     return _this
   }()
 
-  const api = {
-    async fetch(url = '') {
+  const xhr = function() {
+
+    const _this = {}
+    const authDetails = JSON.parse(localStorage.getItem("authDetails"));
+    const userId = escape(authDetails.userId)
+
+    console.log(userId)
+
+    const headers = {
+      'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
+      'apiKey': 'VzNZzWQkodAqqnkEXUsNBhHYt6PcDm6w',
+    }
+
+    async function load(url = '', params = {}) {
       const data = {}
 
-      const res = await fetch(url).then(
+      const res = await fetch(url, params).then(
         res => {
           console.log(res)
 
@@ -104,6 +75,9 @@
           data.statusText = res.ok ? 'success':'error'
           data.statusCode = res.status
 
+          if (data.statusCode === 503) {
+            return res.blob()
+          }
           return res.json()
         },
         err => {
@@ -111,47 +85,100 @@
         }
       )
 
-      return {...data, text: JSON.stringify(res, null, 4)}
-    },
-
-    async getUsers() {
-      const data = await this.fetch('https://jsonplaceholder.typicode.com/sers')
-
-      echo.clear()
-
-      echo.title('[ Get Users ]')
-      data.ok ? echo.success(data):echo.error(data)
-      console.log('data', data)
-      echo.text(data)
-    },
-
-    async getAlbums() {
-      echo.clear()
-      echo.title('[ Get Albums ]')
-      const data = await this.fetch('https://jsonplaceholder.typicode.com/albums')
-      data.ok ? echo.success(data):echo.error(data)
-      console.log('data', data)
-      echo.text(data)
-    },
-
-    async getPosts() {
-      echo.clear()
-      const data = await this.fetch('https://jsonplaceholder.typicode.com/posts')
-      echo.title('1').title('2').title('3')
-      echo.title('[ Get Posts ]')
-      echo[data.ok ? 'success' : 'error'](data)
-      console.log('data', data)
-      echo.text(data)
-
+      return {...data, res}
     }
-  }
 
-  // echo.init()
+    Object.assign(_this, {
+      get userName() {
+        return `${authDetails.firstName} ${authDetails.lastName}`
+      },
 
-  document.querySelectorAll('.btn-action').forEach(el => {
-    //console.log(el)
-    el.addEventListener('click', e => {
-      api[el.getAttribute('data-action')]()
+      getDefaultAddresses: async() => {
+        const data = await load(`https://hermestest-intg.apigee.net/enterprise-user-account-api/v1/users/${userId}/addresses`, {
+          method: 'get',
+          headers: headers
+        })
+
+        echo.clear()
+        echo
+          .title('[ Get Default Addresses ]')
+          .status(data)
+          .text(data)
+      },
+
+      deleteDefaultAddresses: async() => {
+        const data = await load(`https://hermestest-intg.apigee.net/enterprise-user-account-api/v1/users/${userId}/addresses`, {
+          method: 'get',
+          headers: headers
+        })
+
+        const arr = data.res.addresses?.map(el => el.id)
+
+        echo.clear()
+        echo
+          .title('-- Delete Default Addresses --')
+          .status(data)
+          .title('-- Addresses --')
+          .text(arr)
+          //.text(data)
+
+        console.log(data.res, arr)
+      },
+
+      getUsers: async () => {
+        const data = await load('https://jsonplaceholder.typicode.com/sers')
+        echo.clear()
+        echo
+          .title('[ Get Users ]')
+          .status(data)
+          .text(data)
+
+        console.log('data', data)
+      },
+
+      getAlbums: async () => {
+        const data = await load('https://jsonplaceholder.typicode.com/albums')
+        echo.clear()
+        echo
+          .title('[ Get Albums ]')
+          .status(data)
+          .text(data)
+
+        console.log('data', data)
+      },
+
+      getPosts: async () => {
+        const data = await load('https://jsonplaceholder.typicode.com/posts')
+        echo.clear()
+        echo
+          .title('[ Get Posts ]')
+          .status(data)
+          .text(data)
+
+        console.log('data', data)
+      }
     })
-  })
+
+    return _this
+  }()
+
+  const ui = function() {
+    const hau = document.querySelector('.herm-api-ui')
+    const userName = hau.querySelector('.user')
+    userName.textContent = xhr.userName
+
+    let selectedButton = null
+
+    hau.querySelectorAll('.btn-action').forEach(el => {
+      el.addEventListener('click', e => {
+        selectedButton?.classList.remove('selected')
+        selectedButton = e.target
+        selectedButton.classList.add('selected')
+
+        xhr[el.getAttribute('data-action')]()
+      })
+    })
+
+    xhr.deleteDefaultAddresses()
+  }()
 })()
